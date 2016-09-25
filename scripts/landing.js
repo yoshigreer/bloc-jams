d03ac9e85fa666b786f3c411405926d265bbbb4b
yoshigreer <yoshigreer@gmail.com>
2016-09-24 18:53:43 -0600

    assignment 8 done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,15 @@
 var animatePoints = function() {
 
-    var points = document.getElementsByClassName('point');
-    for (var i = 0; i < ) {
-      var revealPoint = function(i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      }
-    };
+  var points = document.getElementsByClassName('point');
+  for (var i = 0; i < point.length; i++) {
+    var revealPoint = function(i) {
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
 
-      revealPoint();
+    revealPoint();
+  };
 
-    };
+};
