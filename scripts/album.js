fdc5012091fae1d961ecdc375426ac5f11fb258a
Trenton Greer <yoshigreer@gmail.com>
2016-10-11 20:28:04 -0600

    assignment 13 revision complete

--- scripts/album.js
+++ scripts/album.js
@@ -126,8 +126,7 @@ window.onload = function() {
 
   songListContainer.addEventListener('mouseover', function(event) {
     if (event.target.parentElement.className === 'album-view-song-item') {
-      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-
+    
       var songItem = getSongItem(event.target);
       var songItemNumber = songItem.getAttribute('data-song-number');
 
